45f84ceab5459ac57cb06a47cabdd092e095c27e
ehylam <48621527+ehylam@users.noreply.github.com>
2022-03-20 15:38:43 +1100

    GLSL vectors and fragment testing

--- lib/utils/shaders/fragmentShader.glsl
+++ lib/utils/shaders/fragmentShader.glsl
@@ -1,10 +1,15 @@
 uniform float time;
 uniform vec2 hover;
 
+varying float vRandom;
+varying float zPosition;
+
 void main() {
+  float z = gl_FragCoord.z / gl_FragCoord.w;
+  float noise = 0.5 + 0.5 * sin(time + z * 0.02);
+  // gl_FragColor = vec4(0.015, vRandom, 0.14, 1.0);
   gl_FragColor = vec4(0.015, 0.13, 0.14, 1.0);
 
-  // float noise = 0.5 + 0.5 * sin(time + gl_FragCoord.xy.x * 0.1);
-  // gl_FragColor.rgb *= noise;
+  // gl_FragColor.rgb -= noise * 0.01;
 
 }
--- lib/utils/shaders/vertexShader.glsl
+++ lib/utils/shaders/vertexShader.glsl
@@ -1,19 +1,38 @@
 uniform float time;
 uniform vec2 hover;
 uniform float hoverState;
+
+attribute float aRandom;
+
 varying float vNoise;
+varying float vRandom;
+varying float zPosition;
 varying vec2 vUv;
 
+
+
+
 void main() {
-  vec3 newposition = position;
-  float dist = distance(uv,hover);
-  newposition.z += 10.0*sin(dist*10. + time * 0.2);
-  vNoise = sin(dist*10.0 - time);
+  // vec3 newposition = position;
+  // float dist = distance(uv,hover);
+  // newposition.z += 10.0*sin(vRandom*10. + time * 0.2);
 
-  vUv = uv;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4( newposition, 1.0 );
+  // vNoise = sin(dist*10.0 - time);
 
-  gl_Position.y += 0.5*cos(vNoise);
+  // vUv = uv;
+  // gl_Position = projectionMatrix * modelViewMatrix * vec4( newposition, 1.0 );
+
+  // zPosition = newposition.z;
+  // vRandom = aRandom;
 
 
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  modelPosition.z += aRandom * 0.1;
+
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedMatrix = projectionMatrix * viewPosition;
+
+  gl_Position = projectedMatrix;
+
+  vRandom = aRandom;
 }
